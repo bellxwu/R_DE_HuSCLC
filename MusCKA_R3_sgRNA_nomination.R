ea1c0cf435c1fbc2936591877bcea57dd3ba3dd2
Bell Wu <wu.bell@outlook.com>
2025-09-29 16:13:23 -0400

    Added new candidate genes to analysis

--- MusCKA_R3_sgRNA_nomination.R
+++ MusCKA_R3_sgRNA_nomination.R
@@ -66,15 +66,30 @@ cand_sgrnas = counts_norm |>
 FC = cand_sgrnas |> 
   mutate(B6_NRG_IgG = log2((B6_IgG+1) / (NRG_IgG+1)),
          B6_NRG_aCD8 = log2((B6_aCD8+1) / (NRG_IgG+1)))
-# filter for FC < 1
+# filter for FC < 1, for negatively selected
 smal_FC = FC |> 
   filter(B6_NRG_IgG < 1)
-top_grna = smal_FC |> 
+# select out top sgRNAs
+top_n_grna = smal_FC |> 
+  group_by(Gene) |> 
+  dplyr::arrange(B6_NRG_IgG, .by_group = TRUE) |> 
+  filter(grepl("MMP11", sgRNA))
+# filter for FC > 1, for positively selected
+large_FC = FC |> 
+  filter(B6_NRG_IgG > 1)
+# select out top sgRNAs
+top_p_grna = large_FC |> 
   group_by(Gene) |> 
   dplyr::arrange(B6_NRG_IgG, .by_group = TRUE)
-# take the first two
-keep = ((seq_along(top_grna$sgRNA) - 1) %% 5) < 2
-select_grnas = top_grna$sgRNA[keep]
+# take the first two (from neg)
+keep = ((seq_along(top_n_grna$sgRNA) - 1) %% 5) < 2
+select_n_grnas = top_n_grna$sgRNA[keep]
+# take the first two (from pos)
+keep = ((seq_along(top_p_grna$sgRNA) - 1) %% 5) < 2
+select_p_grnas = top_p_grna$sgRNA[keep]
+# combine arrays:
+select_grnas = c(select_n_grnas, select_p_grnas)
+
 # 4.1  load and get sequences ---------------------------------------------------
 setwd("~/R_programming/R_MuSCK_Library/")
 grna_seq = read.table("liu_lab_musck_library_a_grna_sequences.txt", header = TRUE)
--- hits_prevalence_George2015.R
+++ hits_prevalence_George2015.R
@@ -2,12 +2,14 @@
 # Goal is to see if the gene hits from muscka and DEGs are high in prevalence.
 # Author: Bell Wu
 # Date created: 2025.09.17
+# Changelog:
+# 2025.09.29 - added 7.0 for analysis of the ver2 candidates to be presented and further validated.
 
 library(tidyverse)
 library(ggthemes)
 library(RColorBrewer)
 
-# 1.0 set wd and load all dfs ----
+# 1.0 set wd and load all dfs ----------------------------------------------------------
 # Load George dataset 
 setwd("~/R_programming/R_RPP_macrophage_adenosine/")
 George = read.csv("George_nodup.csv") # data is in FPKM
@@ -35,7 +37,7 @@ setwd("~/R_programming/R_CCLE/")
 CCLE = read.csv("CLEAN_SCLC_CCLE.csv")
 colnames(CCLE)[1] = "Gene"
 
-# 2.0 select candidate hits from dataset ----
+# 2.0 select candidate hits from dataset ----------------------------------------------------------
 # 2.1 for candidate hits
 candidates = c("PEO1", "DDX23", "TAF7", "NRK", "PECAM1", "SLC6A11", "FBXO11", "KDM1A")
 # from George data set
@@ -59,7 +61,7 @@ candidates_100 = muscka_100$id
 CCLE_hits = CCLE |> 
   filter(Gene %in% candidates_100)
 
-# 3.0 Average expression levels -----
+# 3.0 Average expression levels ---------------------------------------------------------------------------------
 # for chosen hits
 CCLE_means = CCLE_hits |> 
   transmute(Gene = Gene,
@@ -70,10 +72,10 @@ CCLE_means = CCLE_hits |>
   transmute(Gene = Gene,
             Row_means = rowMeans(CCLE_hits[-1]))
 
-# 4.0 plot candidate hit levels across patients -----
+# 4.0 plot candidate hit levels across patients -----------------------------------------------------------
 # Purpose of this section is to plot the candidate hits across the different datasets
 
-# 4.1 Plot out the candidate hits for CCLE -------
+# 4.1 Plot out the candidate hits for CCLE -------------------------------------------------------------
 s_cols = colnames(CCLE_hits)[-1] # take samples 
 d_candidate = CCLE_hits |>  # create a df_long
   pivot_longer(cols = s_cols,
@@ -95,7 +97,7 @@ ggsave("density_CCLE_RNA_hits.png",
        height = 3,
        width = 4)
 
-# 4.2 plot from George_2015 dataset -------
+# 4.2 plot from George_2015 dataset -------------------------------------------------------------
 s_cols = colnames(George_hits)[-1] # take samples 
 d_candidate = George_hits |>  # create a df_long
   pivot_longer(cols = s_cols,
@@ -134,7 +136,7 @@ potential_hits = read.csv("mus_DEG_combined_hits.csv") # read csv for combined h
 head(potential_hits)
 candidates = potential_hits$id # take gene names
 
-# 5.1 Pull candidate genes from dataset ------
+# 5.1 Pull candidate genes from dataset ------------------------------------------------------------
 # from George dataset
 George_hits = George |> 
   filter(Gene %in% candidates, !Gene %in% c("NRK", "TAP1", "SLC6A11"))
@@ -144,7 +146,7 @@ CCLE_hits = CCLE |>
   filter(Gene %in% candidates, !Gene %in% c("NRK", "TAP1", "SLC6A11"))
 dim(CCLE_hits)
 
-# 5.2 plot density/histogram plots from dataset ----
+# 5.2 plot density/histogram plots from dataset ------------------------------------------------------
 s_cols = colnames(CCLE_hits)[-1] # take samples 
 d_candidate = CCLE_hits |>  # create a df_long
   pivot_longer(cols = s_cols,
@@ -196,12 +198,12 @@ ggsave("histogram_George_RNA_hits_NE_total.png",
        height = 3,
        width = 4.5)
 
-# 6.0 Analysis of potential hits (MusCKA < 0.2) ------
+# 6.0 Analysis of potential hits (MusCKA < 0.2) ---------------------------------------------------------
 # Purpose is to pull hits of < 0.2 FDR
 setwd("~/R_programming/R_MuSCK_Library/MusCKA-run3/Ver2_analyses/comparative_analysis/")
 candidates = muscka_0.2$id
 
-# 6.1 Pull data from datasets and remove duplicates: ------
+# 6.1 Pull data from datasets and remove duplicates: ------------------------------------------------------
 # from George dataset
 George_hits = George |> 
   filter(Gene %in% candidates, !Gene %in% c("TAP1", "B2M", "HNRNPK", "DDX23", "TICRR", "PECAM1", "AURKB", "TAF7"))
@@ -211,7 +213,7 @@ CCLE_hits = CCLE |>
   filter(Gene %in% candidates, !Gene %in% c("TAP1", "B2M", "HNRNPK", "DDX23", "TICRR", "PECAM1", "AURKB", "TAF7"))
 dim(CCLE_hits)
 
-# 6.2 Plot density from CCLE -------
+# 6.2 Plot density from CCLE -------------------------------------------------------------
 s_cols = colnames(CCLE_hits)[-1] # take samples 
 d_candidate = CCLE_hits |>  # create a df_long
   pivot_longer(cols = s_cols,
@@ -263,3 +265,70 @@ ggsave("histogram_George_RNA_hits_mus0.2.png",
        height = 3,
        width = 4.5)
 
+# 7.0 Selection of candidate genes: Density and histograms: ------------------------------------------------------
+# 7.1 for candidate hits
+candidates = c("PEO1", "DDX23", "TAF7", "SETD1B", "JAK1", "MMP11")
+# from George data set
+George_hits = George |> 
+  filter(Gene %in% candidates)
+# from CCLE
+CCLE_hits = CCLE |> 
+  filter(Gene %in% candidates)
+
+# 7.2 for top 100 neg hits in CCLE
+candidates_100 = muscka_100$id
+CCLE_hits = CCLE |> 
+  filter(Gene %in% candidates)
+
+s_cols = colnames(CCLE_hits)[-1] # take samples 
+d_candidate = CCLE_hits |>  # create a df_long
+  pivot_longer(cols = s_cols,
+               names_to = "samples",
+               values_to = "expression")
+# plot density for the candidate genes selected previously in CCLE
+d_p = ggplot(data = d_candidate, mapping = aes(x = expression, fill = Gene)) +
+  geom_density(alpha = 0.75,
+               linewidth = 0.5) +
+  scale_fill_brewer(palette = "Set3") +
+  # scale_color_brewer(palette = "Set3") +
+  theme_few() +
+  ggtitle("Density plots of RNA \nexpression in CCLE")
+# save plot
+ggsave("density_CCLE_RNA_candidates-ver2.png",
+       path = "~/R_programming/R_DE_HuSCLC/Analysis_PNGs/",
+       plot = d_p,
+       dpi = 300,
+       height = 3,
+       width = 4)
+
+# 7.3 histogram from George_2015 dataset -------------------------------------------------------------
+s_cols = colnames(George_hits)[-1] # take samples 
+d_candidate = George_hits |>  # create a df_long
+  pivot_longer(cols = s_cols,
+               names_to = "samples",
+               values_to = "expression")
+# set bins
+breaks = c(seq(0, 70, by = 10), Inf)
+d_candidate = d_candidate |> 
+  mutate(bin_expression = cut(expression, breaks = breaks, right = FALSE,
+                              labels = c("0–10", "10–20", "20–30", "30–40", 
+                                         "40–50", "50–60", "60–70", "70+")))
+# plot histogram for the candidate genes selected previously in George
+h_p = ggplot(data = d_candidate, mapping = aes(x = bin_expression, fill = Gene)) +
+  geom_bar(position = "dodge",
+           color = "black",
+           linewidth = 0.25,
+           stat = "count") + 
+  scale_fill_brewer(palette = "Set3") +
+  theme_few() +
+  ggtitle("Histogram of RNA expression \nin George 2015") +
+  xlab(NULL) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+
+# save plot
+ggsave("histogram_George_RNA_candidate-ver2.png",
+       path = "~/R_programming/R_DE_HuSCLC/Analysis_PNGs/",
+       plot = h_p,
+       dpi = 300,
+       height = 3,
+       width = 4.5)
